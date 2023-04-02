8cf0e17370011baec4330ecc0594e0aad60dca83
amolin09 <alex.molina5829@gmail.com>
2023-04-02 18:39:44 -0500

    added elements in dom

--- main.js
+++ main.js
@@ -22,11 +22,13 @@ const displayDate = () => {
 
 const turnNumToString = (number) => {
   console.log(number + " is a " + typeof number + " data type.")
+  document.getElementById("num-to-string1").innerHTML = number + " is a " + typeof number + " data type."
   number = number.toString()
   console.log(number + " is now a " + typeof number + " data type.")
+  document.getElementById("num-to-string2").innerHTML = number + " is now a " + typeof number + " data type."
 }
 
-turnNumToString(7)
+// turnNumToString(7)
 
 
 
@@ -34,8 +36,10 @@ turnNumToString(7)
 
 const turnStringToNum = (text) => {
   console.log(text + " is a " + typeof text + " data type.")
+  document.getElementById("string-to-num1").innerHTML = text + " is a " + typeof text + " data type."
   text = Number(text)
   console.log(text + " is now a " + typeof text + " data type.")
+  document.getElementById("string-to-num2").innerHTML = text + " is now a " + typeof text + " data type."
 }
 
 // turnStringToNum("6")
@@ -52,41 +56,56 @@ const turnStringToNum = (text) => {
 const displayDataType = (someVariable) =>{
   const dataType = typeof someVariable
   console.log(someVariable + " is a " + dataType + " data type.")
+  document.getElementById("display-data-type").innerHTML = someVariable + " is a " + dataType + " data type."
 }
 
   
 // Write a JavaScript program that adds 2 numbers together.
 
 const addNumbers = (x, y) => {
+  x = Number(x)
+  y = Number(y)
   let sum = x + y
   console.log(`The sum of ${x} and ${y} equals ${sum}.`)
+  document.getElementById('sum-output').innerHTML = `The sum of ${x} and ${y} equals ${sum}.`
 }
 
-addNumbers(6, 22)
+// addNumbers(6, 22)
 
 // Write a JavaScript program that runs only when 2 things are true.
 
-const bothTruthChecker = (check1, check2) =>{
-  if(check1 && check2){
-    console.log("These values are both true.")
+const bothEvenCheck = (num1, num2) =>{
+  num1 = Number(num1)
+  num2 = Number(num2)
+  if((num1%2==0)&& (num2%2==0)){
+    console.log("Both of these numbers are even.")
+    document.getElementById('even-output').innerHTML = "Both of these numbers are even."
+  }
+  else{
+    document.getElementById('even-output').innerHTML = " ";
   }
 
 }
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
 
-const eitherTruthChecker = (check1, check2) =>{
-  if(check1 || check2){
-    console.log("At least one of these values is true.")
+const membershipCheck = (goldMember, silverMember) =>{
+  let price = 10
+  if(goldMember || silverMember){
+    console.log(`You are permitted entry`)
+    document.getElementById('membership-output').innerHTML = "You are permitted entry."
   }
- 
-}
+  
+ }
+
+ membershipCheck(false, false)
 
 // Write a JavaScript program that runs when both things are not true.  
 
 const falseChecker = (check1, check2) =>{
   if(!check1 && !check2){
     console.log("Both of these are false.")
+    document.getElementById('false-output').innerHTML = 'Both of these are false.'
   }
 }
 
